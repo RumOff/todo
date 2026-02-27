5782ee340e4ffa8ea2ab6b0d76d510fc80c99eba
RumOff <rum_sakamoto@icloud.com>
2026-02-27 14:39:21 +0000

    userテーブルの作成

--- src/app/Models/Todo.php
+++ src/app/Models/Todo.php
@@ -10,9 +10,14 @@ class Todo extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'category_id',
         'content'
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 
     public function category(){
         return $this->belongsTo(Category::class);
